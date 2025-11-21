10ce888840625e171bbffe94e8256d0077e14ddb
Lucas Astorian <lucasastorian@gmail.com>
2025-11-21 17:08:49 -0500

    Version bump

--- src/sec2md/__init__.py
+++ src/sec2md/__init__.py
@@ -10,7 +10,7 @@ from sec2md.chunker.chunker import Chunker
 from sec2md.parser import Parser
 from sec2md.section_extractor import SectionExtractor
 
-__version__ = "0.1.15"
+__version__ = "0.1.16"
 __all__ = [
     "convert_to_markdown",
     "parse_filing",
